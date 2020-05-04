eebab05e1c8e8a79c2bb6f3262f0cbf71d32bf4f
Damir92 <home_damir92@mail.ru>
2020-05-04 14:05:10 +0300

    install dependencies

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import MainScreen from "./components/main-screen/main-screen.jsx";
+
+ReactDOM.render(
+  <MainScreen />,
+  document.querySelector(`#root`)
+);
